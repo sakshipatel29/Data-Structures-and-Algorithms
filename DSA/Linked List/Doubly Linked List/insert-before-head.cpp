42f60217e517339d35f082331edbd20b2797bbf2
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-04-28 15:10:56 -0500

    insert in doubly linked list

---
+++ DSA/Linked List/Doubly Linked List/insert-before-head.cpp
@@ -0,0 +1,60 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* back;
+    public:
+        Node(int data1, Node* next1, Node* back1){
+            data = data1;
+            next = next1;
+            back = back1;
+        }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+            back = nullptr;
+        }
+};
+
+Node* ConvertArrayToDLL(vector<int>& arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void PrintDLL(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* InsertBeforeHead(Node* head, int val){
+    Node* newNode = new Node(val, head, nullptr);
+    head->back = newNode;
+    return newNode;
+}
+
+int main() {
+    vector<int> arr = {12,0,8,7};
+    Node* head = ConvertArrayToDLL(arr);
+    head = InsertBeforeHead(head, 10);
+    PrintDLL(head);
+    return 0;
+}
+
+// Output: 10 12 0 8 7
---
+++ DSA/Linked List/Doubly Linked List/insert-before-tail.cpp
@@ -0,0 +1,66 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* back;
+    public:
+        Node(int data1, Node* next1, Node* back1){
+            data = data1;
+            next = next1;
+            back = back1;
+        }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+            back = nullptr;
+        }
+};
+
+Node* ConvertArrayToDLL(vector<int>& arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void PrintDLL(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* InsertBeforeTail(Node* head, int val){
+    Node* tail = head;
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+    Node* prev = tail->back;
+    Node* newNode = new Node(val,tail,prev);
+    prev->next = newNode;
+    tail->back = newNode;
+    return head;
+}
+
+int main() {
+    vector<int> arr = {12,0,8,7};
+    Node* head = ConvertArrayToDLL(arr);
+    head = InsertBeforeTail(head, 10);
+    PrintDLL(head);
+    return 0;
+}
+
+// Output: 12 0 8 10 7
