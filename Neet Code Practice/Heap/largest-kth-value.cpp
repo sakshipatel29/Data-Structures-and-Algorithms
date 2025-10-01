b027f9e2154709aab9b423e4964454fdc1454e0e
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-10-01 17:06:41 -0500

    kth largest value in given array

---
+++ Neet Code Practice/Heap/largest-kth-value.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+//TC: O(NlogN)
+//SC: O(N)
+
+class KthLargest {
+    public:
+        int k;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        KthLargest(int k, vector<int>& nums) {
+            this->k = k;
+            for(int i = 0; i < nums.size(); i++){
+                add(nums[i]);
+            }
+        }
+        
+        int add(int val) {
+            if(pq.size() < k || pq.top() < val){
+                pq.push(val);
+                if(pq.size() > k){
+                    pq.pop();
+                }
+            }
+            return pq.top();
+        }
+    };
